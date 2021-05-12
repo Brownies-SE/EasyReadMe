961fee46b8bd10e9c1d691bca76719c20ba2c363
Josh Brown <brownjoshalan@gmail.com>
2021-05-12 13:38:49 -0600

    Main functionality now works and write to the README file

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# EasyReadMe
+# jdjd
+  jdjd
+
+  # Table of Contents
+  *Installation
+  *Usage
+  *License
+  *Contributors
+  *Test
+  *Questions
+
+  # Installation
+  ksks
+  # Usage
+  kjn
+  # License
+  undefined
+  # Contributors
+  sdjks
+  # Tests
+  undefined
+  # Questions
+  iks
--- index.js
+++ index.js
@@ -1,10 +1,65 @@
 // TODO: Include packages needed for this application
+const getMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "github",
+      message: "What is your github username? ",
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "What is the name of the project? ",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please add a description of your project: ",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Installation Instructions: ",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What is the project used for: ",
+    },
+    {
+      type: "input",
+      name: "License",
+      message: "Name all Licenses: ",
+    },
+    {
+      type: "input",
+      name: "contributors",
+      message: "Name all contributors: ",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "Enter tests: ",
+    },
+    {
+      type: "input",
+      name: "questions",
+      message: "Enter Questions: ",
+    },
+  ])
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  .then((response) => {
+    const pageContent = generateMarkdown(response);
+    fs.writeFile("README.md", pageContent, (err) =>
+      err ? console.log(err) : console.log("It worked")
+    );
+  });
 
 // TODO: Create a function to initialize app
 function init() {}
